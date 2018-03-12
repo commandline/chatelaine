4729c139aee2ffba97e834f458576b0907ffd5bd
Thomas Gideon <cmdln@thecommandline.net>
2018-03-12 17:24:12 -0400

    Replace path parameter handling.

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ extern crate futures;
 extern crate hyper;
 #[macro_use]
 extern crate log;
+extern crate luminal_pathparam;
 extern crate luminal_router;
 //extern crate r2d2;
 //extern crate r2d2_diesel;
@@ -17,6 +18,7 @@ use hyper::header::ContentLength;
 use hyper::server::{self, Request, Response};
 use luminal_router::{Router, ServiceFuture};
 
+use std::collections::HashMap;
 //use std::env;
 
 //mod auth;
@@ -25,21 +27,36 @@ use luminal_router::{Router, ServiceFuture};
 //use self::db::DieselMiddleware;
 
 pub fn route() -> Router {
-    let mut router = Router::new();
+    let router = Router::new();
 
-    router
+    let router = router
         .get("/", server::service_fn(handler))
-        .expect("Should have been able to add index route");
+        .expect("Should have been able to add index route")
+        .get("/:query", server::service_fn(handler))
+        .expect("Should have been able to add index route with param");
 
     debug!("Routes added.");
     router
 }
 
-fn handler(_req: Request) -> ServiceFuture {
-    info!("Index handler");
-    let msg = String::from("index");
+fn handler(req: Request) -> ServiceFuture {
+    info!("Index handler: '{}'", req.path());
+    let query = if req.path() == "/" {
+        info!("Default");
+        String::from("Hello")
+    } else {
+        let params =
+            luminal_pathparam::parse("/:query", req.path()).collect::<HashMap<&str, &str>>();
+        if let Some(query) = params.get(":query") {
+            info!("Queried: {}", query);
+            (*query).to_owned()
+        } else {
+            info!("Default");
+            String::from("Hello")
+        }
+    };
     let response = Response::new()
-        .with_header(ContentLength(msg.len() as u64))
-        .with_body(msg);
+        .with_header(ContentLength(query.len() as u64))
+        .with_body(query);
     Box::new(futures::future::ok(response))
 }
